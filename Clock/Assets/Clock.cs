619aeef76bed4faa312c32c76e6c7db27a1364a2
Systran <47870746+systran20@users.noreply.github.com>
2019-03-20 17:11:40 +0300

    Add files via upload

---
+++ Clock/Assets/Clock.cs
@@ -0,0 +1,45 @@
+﻿using System;  //.net tarih saat kütüphanesi için gerekli
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hT;    //hours
+    public Transform mT;    //minutes
+    public Transform sT;    //seconds 
+    public bool secenek;
+
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;    
+
+    void Awake()
+    {   //görüntü oluşturulmadan önce ekranda ibrelerin doğru konumda olmasını sağlar
+        DateTime time = DateTime.Now;
+        hT.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        mT.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        sT.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+    void Update()
+    {
+        if (secenek)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+    void UpdateContinuous () {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hT.localRotation =  Quaternion.Euler(0f, (float) time.TotalHours * degreesPerHour, 0f);
+        mT.localRotation = Quaternion.Euler(0f, (float) time.TotalMinutes * degreesPerMinute, 0f);
+        sT.localRotation = Quaternion.Euler(0f, (float) time.TotalSeconds * degreesPerSecond, 0f);		
+	}
+	void UpdateDiscrete () {
+		DateTime time = DateTime.Now;
+        hT.localRotation =  Quaternion.Euler(0f, time.Hour * degreesPerHour + time.Minute / 2f, 0f);
+        mT.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute + time.Second/10f, 0f);
+        sT.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+	}
+}
---
+++ Clock/Assets/MouseFollower.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    public bool flashLightOn = true;
+    public Light light;
+    public Transform tf;    
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        //Checks if the boolean is true or false.
+        if (flashLightOn == true)
+        {
+            light.enabled=true;
+        }
+        else
+        {
+            light.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashLightOn = !flashLightOn;
+        }
+
+    }
+    void LateUpdate()
+    {
+        FollowMousePosition();
+    }
+    void FollowMousePosition()
+    {
+        tf.rotation = Quaternion.Euler(new Vector3(Input.mousePosition.x , Input.mousePosition.y , 0f));
+        //this.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //this.transform.position = Camera.main.ViewportToScreenPoint(Input.mousePosition);
+        //this.transform.position = Camera.main.WorldToScreenPoint(Input.mousePosition);
+    }
+}
